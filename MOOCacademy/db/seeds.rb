045a6b757bfa8e0a6f9cc50d9e080efc00f2b61c
greg-Da <gregoiredalmasso@gmail.com>
2019-01-30 23:21:12 +0100

    mooc fini

--- MOOCacademy/db/seeds.rb
+++ MOOCacademy/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+#5.times do 
+#   Cour.create!(title: Faker::Beer.name, description: Faker::ChuckNorris.fact)
+#end
+
+20.times do
+    Lecon.create!(title: Faker::Company.name, body: Faker::HarryPotter.quote, cour_id: rand(1..5))
+end
