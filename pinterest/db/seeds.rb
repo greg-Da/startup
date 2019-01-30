9496534f58f79c154d4d9f4c2a333211f822a8ff
greg-Da <gregoiredalmasso@gmail.com>
2019-01-30 23:31:02 +0100

    pinterest

--- pinterest/db/seeds.rb
+++ pinterest/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+    u = User.create!(name: Faker::Name.name)
+end
+
+40.times do
+    p = Pin.create!(url: Faker::Internet.url, user_id: rand(1..10))
+end
+
+20.times do
+    c = Comment.create!(content: Faker::HarryPotter.quote, user_id: rand(1..10))
+end
